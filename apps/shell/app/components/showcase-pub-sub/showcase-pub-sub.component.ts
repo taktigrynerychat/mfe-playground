02a73e28e07b447ada567b2def21fdd6e514e1cf
Vlad <vladdaking@gmail.com>
2023-11-06 00:05:34 +0200

    Removed redundant code

--- apps/shell/app/components/showcase-pub-sub/showcase-pub-sub.component.ts
+++ apps/shell/app/components/showcase-pub-sub/showcase-pub-sub.component.ts
@@ -1,8 +1,6 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MfeAngularPubSubFeatureComponent } from './mfe-angular-pub-sub-feature/mfe-angular-pub-sub-feature.component';
-import { PubSubService } from '../../services/pub-sub.service';
-import { PubSubTopic } from '../../constants';
 import { MfeReactPubSubFeatureComponent } from './mfe-react-pub-sub-feature/mfe-react-pub-sub-feature.component';
 
 @Component({
@@ -17,14 +15,5 @@ import { MfeReactPubSubFeatureComponent } from './mfe-react-pub-sub-feature/mfe-
     class: 'showcase-pub-sub showcase-page'
   }
 })
-export class ShowcasePubSubComponent implements OnDestroy {
-  private readonly subscriptions: string[] = [];
-  constructor(private readonly p: PubSubService) {
-    this.subscriptions.push(this.p.subscribe(PubSubTopic.CustomEvent, console.log));
-  }
-
-  public ngOnDestroy(): void {
-    this.subscriptions.forEach(subId => this.p.unsubscribe(subId));
-  }
-
+export class ShowcasePubSubComponent {
 }
