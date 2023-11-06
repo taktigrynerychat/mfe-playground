4f254bc2839da2b9850abe47860a3eea21c3b5e4
Vlad <vladdaking@gmail.com>
2023-11-06 23:24:28 +0200

    Fixed root module imports

--- apps/shell/app/app.module.ts
+++ apps/shell/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MfeHeaderComponent } from './components/mfe-header/mfe-header.component';
-import { HomeComponent } from './components/home/home.component';
 import { RouterModule } from '@angular/router';
 import { PubSubModule } from './modules/pubsub.module';
 import { PubSubTopic, ROOT_ROUTES } from './constants';
@@ -18,7 +17,6 @@ import { PubSubTopic, ROOT_ROUTES } from './constants';
     ]),
     BrowserModule,
     MfeHeaderComponent,
-    HomeComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
